c928480eeafa015c6097ff4dbd5512e4c5e50097
yanis370 <yanis.pies@gmail.com>
2025-10-20 10:56:05 +0200

    plein de doc pour tester
    jai ajouter different type de document pour tester et voir ce qu'il etait possible de faire

---
+++ app.js
@@ -0,0 +1,62 @@
+
+//Nombres
+const x = 33
+const y = 33
+const result = x + y
+console.log(result);
+//String
+const titre = "Hello World !"
+console.log(titre)
+//Boolean
+const valeurLogique = true
+console.log(valeurLogique);
+//Tableau
+const table = [2, 15, 4, 10, 19]
+console.log(table)
+console.log("Afficher la deuxieme case du tableau", table[1])
+
+
+
+//Afficher la somme de la deuxieme case du tableu additionnée avec la 4eme case 
+const tableResult = table[1] + table[3];
+console.log(tableResult);
+
+//Conditons
+const valeurAComparer = 6
+if (valeurAComparer > 2) {
+    console.log("oui cest superieur");
+}else if (valeurAComparer == 2)
+{console.log("oui c'est egale a 2")}
+
+else {
+    console.log("Non c'est inferieur");
+}
+
+
+const humain = {
+   nom :"Yanis",
+   poids : "110",
+   taille : "190",
+    famille:{
+        pere:{
+            nom: "jf la menace",
+            age: "54",
+
+        }
+
+   }
+}
+
+console.log (humain)
+
+
+
+const compte = {
+    username: "jimbei370",
+    password: "algerie370",
+}
+
+for(let i =0; i<20; i++) {
+    console.log ("jimbei")
+}
+
---
+++ moyenne.js
@@ -0,0 +1,18 @@
+const notes = [12,15,8,10,14]
+     
+
+
+function calculermoyenne(tab){
+
+    let somme = 0;
+    for (let index = 0; i < tab.length; i++) {
+        somme = somme + tab[i];
+       
+        
+    }
+console.log (somme); 
+
+}
+calculermoyenne
+
+
